03198cc91b7a449d332afd45bc853083c1c50cb4
Branon Eusebio <branoneusebio@Branons-MacBook-Pro.local>
2020-07-20 15:34:59 -0700

    Finished up hero strip styling and mobile responesiveness

--- src/components/header/menu.js
+++ src/components/header/menu.js
@@ -25,7 +25,7 @@ const Menu = (props) => {
                     return <li>
                       <Link 
                         to={`${page.path}`} 
-                        activeStyle={{background: 'linear-gradient(90deg, #F493AA 0%, #F2D1C9 100%)'}}
+                        activeStyle={{background: 'linear-gradient(90deg, #F493AA 0%, #F2D1C9 100%)', outline: 'none', border: 'none'}}
                       >
                         {page.name}
                       </Link>
