2a791221d7218e88ef95eb9e11b5a6187fabacc9
Branon Eusebio <branoneusebio@Branons-MacBook-Pro.local>
2020-07-20 17:04:44 -0700

    Developed most of the services strip with mobile responsiveness

---
+++ src/components/services/services.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import './services.css'
+import heartIcon from '../../images/heart-icon.png'
+import houseIcon from '../../images/house-icon.png'
+import stethoscopeIcon from '../../images/stethoscope-icon.png'
+import healthIcon from '../../images/health-icon.png'
+import ButtonPrimaryAlt from '../buttons/button-primary-alt'
+
+
+const ServicesStrip = () => {
+  return (
+    <div className='ServicesStrip'>
+        <h2>Our Post-Accute Care Services</h2>
+        <div className='card-container'>
+            <div className='card'>
+                <img src={heartIcon} alt="Heart symbol" class='icon' />
+                <h3>Hospice</h3>
+                <p className='introduction-text'>This is a description about a service.</p>
+                <ButtonPrimaryAlt content='Read more' alt='Go to Hospice Page' />
+            </div>
+            <div className='card'>
+                <img src={stethoscopeIcon} alt="Stehoscope symbol" class='icon' />
+                <h3>H.A.R.P.</h3>
+                <p className='introduction-text'>This is a description about a service.</p>
+                <ButtonPrimaryAlt content='Read more' alt='Go to HARP Page' />
+            </div>
+            <div className='card'>
+                <img src={healthIcon} alt="Healthcare cross symbol" class='icon' />
+                <h3>Palliative Care</h3>
+                <p className='introduction-text'>This is a description about a service.</p>
+                <ButtonPrimaryAlt content='Read more' alt='Go to Palliative Care Page' />
+            </div>
+            <div className='card'>
+                <img src={houseIcon} alt="House symbol" class='icon' />
+                <h3>Home Health</h3>
+                <p className='introduction-text'>This is a description about a service.</p>
+                <ButtonPrimaryAlt content='Read more' alt='Go to Home Health Page' />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ServicesStrip
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,15 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import HeroStrip from '../components/hero/hero'
+import AboutStrip from '../components/about/about'
+import ServicesStrip from '../components/services/services'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <HeroStrip />
+    <AboutStrip />
+    <ServicesStrip />
   </Layout>
 )
 
