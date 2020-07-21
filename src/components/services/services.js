425c9855173fbda1cf9ffd791b2a726419d01a28
Branon Eusebio <branoneusebio@Branons-MacBook-Pro.local>
2020-07-21 10:08:55 -0700

    Added minor style adjustments to header and added Link router tags to services buttons

--- src/components/services/services.js
+++ src/components/services/services.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './services.css'
+import {Link} from 'gatsby'
 import heartIcon from '../../images/heart-icon.png'
 import houseIcon from '../../images/house-icon.png'
 import stethoscopeIcon from '../../images/stethoscope-icon.png'
@@ -16,25 +17,33 @@ const ServicesStrip = () => {
                 <img src={heartIcon} alt="Heart symbol" class='icon' />
                 <h3>Hospice</h3>
                 <p className='introduction-text'>This is a description about a service.</p>
-                <ButtonPrimaryAlt content='Read more' alt='Go to Hospice Page' />
+                <Link to='/services/hospice'>
+                    <ButtonPrimaryAlt content='Read more' alt='Go to Hospice Page' />
+                </Link>
             </div>
             <div className='card'>
                 <img src={stethoscopeIcon} alt="Stehoscope symbol" class='icon' />
                 <h3>H.A.R.P.</h3>
                 <p className='introduction-text'>This is a description about a service.</p>
-                <ButtonPrimaryAlt content='Read more' alt='Go to HARP Page' />
+                <Link to='/services/harp'>
+                    <ButtonPrimaryAlt content='Read more' alt='Go to HARP Page' />
+                </Link>
             </div>
             <div className='card'>
                 <img src={healthIcon} alt="Healthcare cross symbol" class='icon' />
                 <h3>Palliative Care</h3>
                 <p className='introduction-text'>This is a description about a service.</p>
-                <ButtonPrimaryAlt content='Read more' alt='Go to Palliative Care Page' />
+                <Link to='/services/palliative-care'>
+                    <ButtonPrimaryAlt content='Read more' alt='Go to Palliative Care Page' />
+                </Link>
             </div>
             <div className='card'>
                 <img src={houseIcon} alt="House symbol" class='icon' />
                 <h3>Home Health</h3>
                 <p className='introduction-text'>This is a description about a service.</p>
-                <ButtonPrimaryAlt content='Read more' alt='Go to Home Health Page' />
+                <Link to='/services/home-health'>
+                    <ButtonPrimaryAlt content='Read more' alt='Go to Home Health Page' />
+                </Link>
             </div>
         </div>
     </div>
---
+++ src/pages/services/home-health.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../../components/layout"
+import SEO from "../../components/seo"
+
+const HomeHealthPage = () => (
+  <Layout>
+    <SEO title="Home Health Page" />
+    <h1>Home Health</h1>
+    <p className='introduction-text'>We're devoted to our patients.</p>
+  </Layout>
+)
+
+export default HomeHealthPage
