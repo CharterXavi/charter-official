d0f428827db65b7e607bd8677aa9cf6355b79063
Branon Eusebio <branoneusebio@Branons-MacBook-Pro.local>
2020-07-29 16:58:18 -0700

    Installed AOS library and applied to buttons

--- src/components/buttons/button-primary.js
+++ src/components/buttons/button-primary.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import './button-primary.css'
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const ButtonPrimary = (props) => {
+    useEffect(() => {
+        AOS.init();
+        AOS.refresh();
+    });
+
     return (
-        <div className='ButtonPrimary'>
+        <div className='ButtonPrimary' data-aos={props.animation} data-aos-duration={props.animationTime}>
             <a href={props.link} className='btn-primary'>
                 {/* if an icon prop is passed, display it */}
                 {props.icon && <img src={props.icon} alt='Button Icon' />}
