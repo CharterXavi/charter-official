e5bf4832d2b91e63e0bd42e1b3bc3fdae9181bbf
Branon Eusebio <branoneusebio@Branons-MacBook-Pro.local>
2020-08-03 13:20:54 -0700

    Created series of components for Team Page Cards that expand to reveal biographies

---
+++ src/components/our-team/team-card.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './team-card.css'
+
+const TeamCard = (props) => {
+
+    const handleClick = () => {
+        props.expandBio(props.id);
+    }
+
+  return (
+    <div className='TeamCard'>
+        <img src={props.image} alt="Team Member" class={`${props.isClicked ? 'enlarge-image' : ''}`} />
+        <div className='card-header'>
+            <h4>{props.name}</h4>
+            <p className='introduction-text'>{props.title}</p>
+        </div>
+        <div className='card-content'>
+            <p className='quote'>{props.quote}</p>
+            <div className={`biography ${props.isClicked ? 'expanded-bio' : ''}`}>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Commodo sagittis, quis eget morbi leo sodales mattis lobortis hac. Sit adipiscing nulla vitae consequat egestas ultricies ultrices interdum.</p>
+                <p>Egestas nisi, pretium mi ultricies lacus habitant cras. Aliquet nunc ultricies risus pretium suspendisse nibh diam. Egestas viverra et vitae ultrices odio non vitae iaculis. Lectus diam nisi, tellus, tortor fames.</p>
+            </div>
+        </div>
+        <div className={`card-expander ${props.isClicked ? 'hide' : ''}`} onClick={handleClick}>
+            {props.isClicked ? <p>Hide</p> : <p>Read more</p>}
+        </div>
+    </div>
+  )
+}
+
+export default TeamCard
--- src/pages/our-team.js
+++ src/pages/our-team.js
@@ -3,8 +3,9 @@ import Layout from "../components/layout";
 import SEO from "../components/seo";
 import teamImage from '../images/our-team-header.png';
 import HeaderStrip from '../components/header-strip/header-strip';
-import OurTeamStrip from '../components/our-team/our-team';
 import LocationsStrip from '../components/locations/locations';
+import TeamGrid from '../components/our-team/team-grid';
+import './our-team.css';
 
 const OurTeamPage = () => (
   <Layout>
@@ -14,7 +15,12 @@ const OurTeamPage = () => (
       title='Our Team'
       headline="We're blessed to get to work with the best"
     />
-    <OurTeamStrip />
+    <div className='intro'>
+      <h2>Exceptional Care & Life Enhancing Services</h2>
+      <p>At Charter Healthcare Group we believe in making a positive change in the lives of our patients and their families. For over ten years, we have been committed to providing exceptional care and life enhancing services to people all throughout the greater Los Angeles area.</p>
+      <p>We have four main service offerings: home health care, palliative care, hospital admission reduction program (H.A.R.P.), and hospice.</p>
+    </div>
+    <TeamGrid />
     <LocationsStrip />
   </Layout>
 )
