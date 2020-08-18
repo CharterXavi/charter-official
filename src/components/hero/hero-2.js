bd1a8a32c54dbc0d0d1026b0b7c4c9ff5477d1e9
Branon Eusebio <branoneusebio@Branons-MacBook-Pro.local>
2020-08-17 17:05:12 -0700

    Updated Hero Strip 2 design and improved mobile experience

--- src/components/hero/hero-2.js
+++ src/components/hero/hero-2.js
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react';
 import iconBlast from '../../images/icon-blast.png';
 import doctorGraphic from '../../images/doctor-graphic.png';
 import ButtonPrimary from '../buttons/button-primary';
+import charterFeather from '../../images/charter-feather.png';
 import './hero-2.css';
 import AOS from "aos";
 import "aos/dist/aos.css";
@@ -56,33 +57,13 @@ const HeroStrip2 = () => {
             <img className='icon-blast' src={iconBlast} alt="Healthcare icons floating around" />
             <img className='doctor-graphic' src={doctorGraphic} alt="Doctor at a desk" />
             <div className='hero-content'>
-                <svg class='homeIcon' xmlns="http://www.w3.org/2000/svg" width="48" height="54" viewBox="0 0 48 54" fill="none" data-aos='fade-up' data-aos-duration='600'>
-                    <g filter="url(#filter1_d)">
-                    <path fill-rule="evenodd" clip-rule="evenodd" d="M25.0403 0.621793C24.2519 -0.207265 22.93 -0.207264 22.1416 0.621793L2.55465 21.2195C1.34344 22.4932 2.24631 24.5977 4.00398 24.5977H5.3064V49.1676H42.3064V24.5977H43.1779C44.9355 24.5977 45.8384 22.4932 44.6272 21.2195L25.0403 0.621793Z" fill="url(#paint0_linear)"/>
-                    </g>
-                    <path fill-rule="evenodd" clip-rule="evenodd" d="M32.6768 32.4044C34.0471 30.0243 33.8979 27.243 32.0891 25.5246C29.967 23.5086 26.3049 23.6627 23.5915 25.772C20.8781 23.6627 17.216 23.5086 15.0939 25.5246C13.2127 27.3117 13.1266 30.2484 14.6778 32.6878C14.6848 32.702 14.692 32.7161 14.6995 32.7303C15.9236 35.0561 18.297 37.9885 20.2877 40.2618C22.0853 42.3147 25.1995 42.3623 27.01 40.3208C28.8893 38.2016 31.1238 35.41 32.5341 32.7303C32.5927 32.619 32.6401 32.5103 32.6768 32.4044Z" fill="white"/>
-                    <defs>
-                    <filter id="filter1_d" x="0" y="0" width="47.1819" height="53.1676" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
-                    <feOffset dy="2"/>
-                    <feGaussianBlur stdDeviation="1"/>
-                    <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
-                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow"/>
-                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape"/>
-                    </filter>
-                    <linearGradient id="paint0_linear" x1="4.31331" y1="49.1676" x2="56.38" y2="29.2002" gradientUnits="userSpaceOnUse">
-                    <stop stop-color="#FFCE94"/>
-                    <stop offset="1" stop-color="#F05F7E"/>
-                    </linearGradient>
-                    </defs>
-                </svg>
-                <h1 data-aos='fade-up' data-aos-duration='900'>
+                <img src={charterFeather} alt="Feather logo" />
+                <h1>Charter Healthcare Group</h1>
+                <h2 data-aos='fade-up' data-aos-duration='900'>
                     We're here for you.
                     <br></br>
                     We're there for you.  
-                </h1>
-                <p className='introduction-text' data-aos='fade-up' data-aos-duration='1000'>Throughout the many stages of post-accute care, we put YOU first.</p>
+                </h2>
                 <ButtonPrimary link='/about' content='Learn More' animation='fade-up' animationTime='1200' />
             </div>
         </div>
