89f598e98952de73450f794f33bfb4b9ffbf5900
Branon Eusebio <branoneusebio@Branons-MBP.nv.charter.com>
2020-08-31 12:19:24 -0700

    Took detour from HARP page to reprogram testimonials carousel

--- src/pages/services/harp.js
+++ src/pages/services/harp.js
@@ -3,13 +3,26 @@ import Layout from "../../components/layout"
 import SEO from "../../components/seo"
 import HeaderStrip from '../../components/header-strip/header-strip';
 import harpImg from '../../images/headers/harp.png';
+import TCM from '../../components/harp/tcm';
+import ACP from '../../components/harp/acp';
+import TestimonialsStrip from '../../components/testimonials/testimonials';
+import LocationsStrip from '../../components/locations/locations';
+import './harp.css';
 
 const HarpPage = () => (
   <Layout>
-    <SEO title="HARP Page" />
-    <HeaderStrip image={harpImg} title='H.A.R.P.' headline='Health and Recovery Plans' />
-    <h1>H.A.R.P.</h1>
-    <p className='introduction-text'>We're devoted to our patients.</p>
+    <div className='HarpPage'>
+      <SEO title="HARP Page" />
+      <HeaderStrip image={harpImg} title='H.A.R.P.' headline='Hospital Admission Reduction Program' />
+      <div className='intro'>
+        <h2>Keeping our patients out of the hospital</h2>
+        <p>Our H.A.R.P. program, or "Hospital Admission Reduction Program", consists of two programs aimed at most effectively preventing our patients from going back to the hospital after they are discharged. This includes our Transitional Care Management (TCM) program and our Advanced Care Planning (ACP) program.</p>
+      </div>
+      <TCM />
+      <ACP />
+      <TestimonialsStrip svg={true} />
+      <LocationsStrip />
+    </div>
   </Layout>
 )
 
