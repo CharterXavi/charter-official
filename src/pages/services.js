027989edccb5c7af88793335a7fa2af7f583d41c
Branon Eusebio <branoneusebio@Branons-MacBook-Pro.local>
2020-09-16 12:20:05 -0700

    Added bereavement page link to services page

--- src/pages/services.js
+++ src/pages/services.js
@@ -6,6 +6,7 @@ import servicesImage from '../images/headers/services-header.png';
 import ServicesStrip from '../components/services/services';
 import LocationsStrip from '../components/locations/locations';
 import TestimonialsStrip from '../components/testimonials/testimonials';
+import ButtonPrimary from '../components/buttons/button-primary';
 import AOS from "aos";
 import "aos/dist/aos.css";
 import './services.css';
@@ -20,21 +21,28 @@ const ServicesPage = () => {
   
   return (
     <Layout>
-      <SEO title="Services" />
-      <HeaderStrip 
-        image={servicesImage}
-        title='Our Services'
-        headline='Exceptional Care & Life Enhancing Services'
-      />
-      <div className='intro'>
-          <h2>Exceptional Care & Life Enhancing Services</h2>
-          <p>At Charter Healthcare Group we believe in making a positive change in the lives of our patients and their families. For over ten years, we have been committed to providing exceptional care and life enhancing services to people all throughout the greater Los Angeles area.</p>
-          <p>We have four main service offerings: home health care, palliative care, hospital admission reduction program (H.A.R.P.), and hospice. </p>
+      <div className='ServicesPage'>
+        <SEO title="Services" />
+        <HeaderStrip 
+          image={servicesImage}
+          title='Our Services'
+          headline='Exceptional Care & Life Enhancing Services'
+        />
+        <div className='intro'>
+            <h2>Exceptional Care & Life Enhancing Services</h2>
+            <p>At Charter Healthcare Group we believe in making a positive change in the lives of our patients and their families. For over ten years, we have been committed to providing exceptional care and life enhancing services to people all throughout the greater Los Angeles area.</p>
+            <p>We have four main service offerings: home health care, palliative care, hospital admission reduction program (H.A.R.P.), and hospice. </p>
+        </div>
+        <ServicesStrip />
+        <div className='intro bereavement'>
+            <h2>Bereavement</h2>
+            <p>At Charter, we stand by our patients and their loved ones wherever possible. An especially important way to do that is through bereavement support and services. When grief and loss occur, life can get a lot harder and we want to make sure we’re helping you navigate it all.</p>
+            <ButtonPrimary content='Visit Bereavement Page' link='/services/bereavement' />
+        </div>
+        <PatientPath />
+        <TestimonialsStrip svg={true} />
+        <LocationsStrip svg={true} />
       </div>
-      <ServicesStrip />
-      <PatientPath />
-      <TestimonialsStrip svg={true} />
-      <LocationsStrip svg={true} />
     </Layout>
   )
 }
