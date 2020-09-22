0f659339c3f02380ba816d2bde6e69338aba703d
Branon Eusebio <branoneusebio@Branons-MacBook-Pro.local>
2020-09-22 13:25:19 -0700

    Designed and developed testimonials page with quotes and partners strip

--- src/pages/about/testimonials.js
+++ src/pages/about/testimonials.js
@@ -4,6 +4,8 @@ import SEO from "../../components/seo";
 import testimonialsImg from '../../images/headers/testimonials.png';
 import HeaderStrip from '../../components/header-strip/header-strip';
 import QuoteMosaic from '../../components/testimonials/quote-mosaic';
+import Partners from '../../components/about/partners';
+import LocationsStrip from '../../components/locations/locations';
 
 import './testimonials.css';
 
@@ -17,8 +19,11 @@ const TestimonialsPage = () => (
         <p>We work hard, day and night, to ensure the highest quality care and the greatest satisfaction for each and every one of our patients. In doing so, nothing motivates us to keep working diligently more than the kind feedback and gratitude from our patients, their famililes, and their loved ones.</p>
         <p>We’re excited to be able to share that joy with you - see for yourself what people have to say about Charter!</p>
         </div>
-
+        <hr/>
         <QuoteMosaic />
+        <h2>Our Partners</h2>
+        <Partners />
+        <LocationsStrip svg={true}/>
     </div>
   </Layout>
 )
