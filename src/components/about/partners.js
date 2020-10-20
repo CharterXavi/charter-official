05dd059adb7e5f44bb14bef140dd1dbb1dc7748b
Branon Eusebio <branoneusebio@Branons-MacBook-Pro.local>
2020-10-20 15:15:21 -0700

    Took detour to standardize two column layouts in layout.css

--- src/components/about/partners.js
+++ src/components/about/partners.js
@@ -1,26 +1,38 @@
 import React from "react";
 import './partners.css';
-import covina from '../../images/covina.png';
-import epic from '../../images/epic.png';
-import valley from '../../images/valley.png';
-import primecare from '../../images/primecare.png';
-import iehp from '../../images/iehp.png';
-import lomalinda from '../../images/lomalinda.png';
-import lakeside from '../../images/lakeside.png';
-import regal from '../../images/regal.png';
+import bcbs from '../../images/partners/bcbs.png';
+import epic from '../../images/partners/epic.png';
+import healthcarePartners from '../../images/partners/healthcare-partners.png';
+import healthcenterPartners from '../../images/partners/healthcenter-partners.png';
+import hollywoodPres from '../../images/partners/hollywood-pres.png';
+import iehp from '../../images/partners/iehp.png';
+import kaiser from '../../images/partners/kaiser.png';
+import LACare from '../../images/partners/lacare.png';
+import LLUH from '../../images/partners/lluh.png';
+import mediCal from '../../images/partners/medical.png';
+import medicare from '../../images/partners/medicare.png';
+import monarch from '../../images/partners/monarch.png';
+import regal from '../../images/partners/regal.png';
+import riverside from '../../images/partners/riverside.png';
 
 const Partners = () => {
 
   return (
     <div className='Partners'>
-        <img src={covina} alt="Company Logo" />
+        <img src={bcbs} alt="Company Logo" />
         <img src={epic} alt="Company Logo" />
-        <img src={valley} alt="Company Logo" />
-        <img src={primecare} alt="Company Logo" />
+        <img src={healthcarePartners} alt="Company Logo" />
+        <img src={healthcenterPartners} alt="Company Logo" />
+        <img src={hollywoodPres} alt="Company Logo" />
         <img src={iehp} alt="Company Logo" />
-        <img src={lomalinda} alt="Company Logo" />
-        <img src={lakeside} alt="Company Logo" />
+        <img src={kaiser} alt="Company Logo" />
+        <img src={LACare} alt="Company Logo" />
+        <img src={LLUH} alt="Company Logo" />
+        <img src={mediCal} alt="Company Logo" />
+        <img src={medicare} alt="Company Logo" />
+        <img src={monarch} alt="Company Logo" />
         <img src={regal} alt="Company Logo" />
+        <img src={riverside} alt="Company Logo" />
     </div>
   )
 }
