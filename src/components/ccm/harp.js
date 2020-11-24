5c251046e8f2f78fddd5f163092ba9c647b80aee
Branon Eusebio <branoneusebio@Branons-MacBook-Pro.local>
2020-11-24 10:51:19 -0800

    Cleaned up the layout on CCM page with two-column classes

--- src/components/ccm/harp.js
+++ src/components/ccm/harp.js
@@ -17,38 +17,56 @@ const HARP = () => {
 
   return (
     <div className='HARP'>
-      <div className='intro'>
-        <h4>Transitional Care Management</h4>
-        <p>Transitional Care Management (TCM) is a program that provides services for individuals who need help during transitions in care from an inpatient setting (including acute hospital, rehabilitation hospital, long-term acute care hospital or skilled nursing facility) to the patient’s home.</p>
-        <p>A Transitional Care Specialist will visit you at the hospital or at home to explain the benefits of the program. Then a home nurse visit will be set up to evaluate your needs.</p>
-        <h6>Goals</h6>
-        <p>The Program can help you:</p>
-        <ul>
-            <li>Stay safe and stable at home to avoid unnecessary visits to the hospital</li>
-            <li>Set goals or your health</li>
-            <li>Prepare for your routine visit to your primary care doctor</li>
-            <li>Answer questions and concerns you may have regarding your condition</li>
-            <li>Learn about your symptoms and how to treat them</li>
-            <li>Teach you about your health condition and your medications</li>
-        </ul>
-      </div>
-      <div className='intro'>
-        <h4>Advanced Care Planning</h4>
-        <p>Our qualified health care professionals work to help you identify your needs, goals and medical treatment preferences so you have a plan in place for advanced care. Once these first steps are completed they will help you select the most appropriate Advanced Care Directive. The goal is to ensure that you select a health care agent that you trust and feel comfortable with and that you receive the treatment you want when you can no longer make those decisions for yourself.</p>
-        <h6>Forms</h6>
-        <p>Two important forms should be completed during this process:</p>
-        <div className='forms'>
-          <div className='form-card' data-aos='fade-up' data-aos-duration='800'>
-            <img src={stethoscopeIcon} alt="Stethoscope" />
-            <h6 className='detail-title'>Physician's Orders for Life-Sustaining Treatment (POLST)</h6>
-            <p className='detail-text'>This is a medical order that tells emergency health care professionals what to do during a medical crisis where the patient cannot speak for him or herself.</p>
-            <ButtonSecondary link={polstForm} download={true} content='Download form' />
+      <div className='two-column'>
+        <div className="title">
+          <h5>Transitional Care Management</h5>
+        </div>
+        <div className="content">
+          <div className="left">
+            <h6>What is TCM?</h6>
+            <p>Transitional Care Management (TCM) is a program that provides services for individuals who need help during transitions in care from an inpatient setting (including acute hospital, rehabilitation hospital, long-term acute care hospital or skilled nursing facility) to the patient’s home.</p>
+            <p>A Transitional Care Specialist will visit you at the hospital or at home to explain the benefits of the program. Then a home nurse visit will be set up to evaluate your needs.</p>
           </div>
-          <div className='form-card' data-aos='fade-up' data-aos-duration='1400'>
-            <img src={healthIcon} alt="Healthcare" />
-            <h6 className='detail-title'>Advanced Healthcare Directive</h6>
-            <p className='detail-text'>You have the right to give instructions about your own health care. You also have the right to name someone else to make health care decisions for you. This form lets you do either or both of these things.</p>
-            <ButtonSecondary link={directiveForm} download={true} content='Download form' />
+          <div className="right">
+            <h6>Goals</h6>
+            <p>The Program can help you:</p>
+            <ul>
+                <li>Stay safe and stable at home to avoid unnecessary visits to the hospital</li>
+                <li>Set goals or your health</li>
+                <li>Prepare for your routine visit to your primary care doctor</li>
+                <li>Answer questions and concerns you may have regarding your condition</li>
+                <li>Learn about your symptoms and how to treat them</li>
+                <li>Teach you about your health condition and your medications</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className='two-column'>
+        <div className="title">
+          <h5>Advanced Care Planning</h5>
+        </div>
+        <div className="content">
+          <div className="left">
+            <h6>What is ACP?</h6>
+            <p>Our qualified health care professionals work to help you identify your needs, goals and medical treatment preferences so you have a plan in place for advanced care. Once these first steps are completed they will help you select the most appropriate Advanced Care Directive. The goal is to ensure that you select a health care agent that you trust and feel comfortable with and that you receive the treatment you want when you can no longer make those decisions for yourself.</p>
+            <h6>Forms</h6>
+            <p>Two important forms should be completed during this process: the Advanced Healthcare Directive and the Physician's Order for Life-Sustaining Treatment (POLST).</p>
+          </div>
+          <div className="right">
+            <div className='forms'>
+              <div className='form-card' data-aos='fade-up' data-aos-duration='1400'>
+                <img src={healthIcon} alt="Healthcare" />
+                <h6 className='detail-title'>Advanced Healthcare Directive</h6>
+                <p className='detail-text'>You have the right to give instructions about your own health care. You also have the right to name someone else to make health care decisions for you. This form lets you do either or both of these things.</p>
+                <ButtonSecondary link={directiveForm} download={true} content='Download form' />
+              </div>
+              <div className='form-card' data-aos='fade-up' data-aos-duration='800'>
+                <img src={stethoscopeIcon} alt="Stethoscope" />
+                <h6 className='detail-title'>Physician's Orders for Life-Sustaining Treatment (POLST)</h6>
+                <p className='detail-text'>This is a medical order that tells emergency health care professionals what to do during a medical crisis where the patient cannot speak for him or herself.</p>
+                <ButtonSecondary link={polstForm} download={true} content='Download form' />
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/pages/services/ccm.js
+++ src/pages/services/ccm.js
@@ -13,12 +13,20 @@ const CCMPage = () => (
     <div className='CCMPage'>
       <SEO title="CCM" />
       <HeaderStrip2 image={harpImg} title='C.C.M.' headline='Complex Care Management' />
-      <div className='intro'>
-        <h4>Keeping our patients out of the hospital</h4>
-        <h6>What is CCM?</h6>
-        <p>Complex Care Management, or CCM, is a collection of programs and initiatives designed for patients with serious medical needs, who are susceptible to more frequent hospitalization. The goal of CCM is to reduce hospitalizations and emergency room visits by bridging gaps in care through in-home provider and nursing visits, connecting members to specialists, identifying social determinants of care, providing medication reconciliation and medication changes as indicated as well as providing 24/7 access to care through our inhouse triage team. In short, this program aims to improve communication and coordination for patients' care as they are discharged from hospitals to help avoid readmission.</p>
-        <h6>Hospital Admission Reduction Program (HARP)</h6>
-        <p>Our CCM program accomplishes its goals primarily through our H.A.R.P. model (Hospitalization Admission Reduction Program) consisting of Transitional Care Management (TCM) and Advanced Care Planning (ACP).</p>
+      <div className='two-column'>
+        <div className="title">
+          <h4>Keeping our patients out of the hospital</h4>
+          <hr/>
+        </div>
+        <div className="content">
+          <div className="left">        
+            <h6>What is CCM?</h6>
+            <p>Complex Care Management, or CCM, is a collection of programs and initiatives designed for patients with serious medical needs, who are susceptible to more frequent hospitalization. The goal of CCM is to reduce hospitalizations and emergency room visits by bridging gaps in care through in-home provider and nursing visits, connecting members to specialists, identifying social determinants of care, providing medication reconciliation and medication changes as indicated as well as providing 24/7 access to care through our inhouse triage team. In short, this program aims to improve communication and coordination for patients' care as they are discharged from hospitals to help avoid readmission.</p></div>
+          <div className="right">
+            <h6>Hospital Admission Reduction Program (HARP)</h6>
+            <p>Our CCM program accomplishes its goals primarily through our H.A.R.P. model (Hospitalization Admission Reduction Program) consisting of Transitional Care Management (TCM) and Advanced Care Planning (ACP).</p>
+          </div>
+        </div>
       </div>
       <HARP />
       <CCMData />
