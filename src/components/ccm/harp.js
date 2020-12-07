23b5110de9e7812ad8fa1d1228dce3394e93d207
Branon Eusebio <branoneusebio@Branons-MacBook-Pro.local>
2020-12-07 15:23:05 -0800

    Replaced icons on HARP page

--- src/components/ccm/harp.js
+++ src/components/ccm/harp.js
@@ -57,13 +57,61 @@ const HARP = () => {
         </div>
         <div className='forms'>
               <div className='form-card' data-aos='fade-up' data-aos-duration='800'>
-                <img src={healthIcon} alt="Healthcare" />
+                <svg xmlns="http://www.w3.org/2000/svg" width="54" viewBox="0 0 54 54" fill="none" className='icon'>
+                    <g filter="url(#healthcare)">
+                        <circle cx="27" cy="25" r="24" fill="url(#healthcare1)" stroke="#363F55" strokeWidth="2"/>
+                        <path fillRule="evenodd" clipRule="evenodd" d="M32 12C32 10.8954 31.1046 10 30 10H24C22.8954 10 22 10.8954 22 12V18C22 19.1046 21.1046 20 20 20H14C12.8954 20 12 20.8954 12 22V28C12 29.1046 12.8954 30 14 30H20C21.1046 30 22 30.8954 22 32V38C22 39.1046 22.8954 40 24 40H30C31.1046 40 32 39.1046 32 38V32C32 30.8954 32.8954 30 34 30H40C41.1046 30 42 29.1046 42 28V22C42 20.8954 41.1046 20 40 20H34C32.8954 20 32 19.1046 32 18V12Z" fill="white"/>
+                    </g>
+                    <defs>
+                        <filter id="healthcare" x="0" y="0" width="54" height="54" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                            <feFlood floodOpacity="0" result="BackgroundImageFix"/>
+                            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+                            <feOffset dy="2"/>
+                            <feGaussianBlur stdDeviation="1"/>
+                            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+                            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow"/>
+                            <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape"/>
+                        </filter>
+                        <linearGradient id="healthcare1" x1="4.67857" y1="50" x2="62.7587" y2="24.6388" gradientUnits="userSpaceOnUse">
+                            <stop offset="0.109375" stopColor="#F14D70"/>
+                            <stop offset="0.317708" stopColor="#FF008A"/>
+                            <stop offset="0.520833" stopColor="#FF446C"/>
+                            <stop offset="0.869792" stopColor="#FFB016"/>
+                        </linearGradient>
+                    </defs>
+                </svg>
                 <h6 className='detail-title'>Advanced Healthcare Directive</h6>
-                <p className='detail-text'>You have the right to give instructions about your own health care. You also have the right to name someone else to make health care decisions for you. This form lets you do either or both of these things.</p>
+                <p className='detail-text'>You have the right to give instructions about your own health care and to name someone else to make health care decisions for you. This form lets you do either or both of these things.</p>
                 <ButtonSecondary link={directiveForm} download={true} content='Download form' />
               </div>
               <div className='form-card' data-aos='fade-up' data-aos-duration='1400'>
-                <img src={stethoscopeIcon} alt="Stethoscope" />
+                <svg width="55" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg" className='icon'>
+                  <g filter="url(#stetho)">
+                    <circle cx="32" cy="30" r="29" fill="url(#stetho1)" stroke="#363F55" strokeWidth="2"/>
+                    <path d="M27.7969 8.39999C16.7068 23.1554 26.0004 28.2 26.0004 28.2M26.0004 28.2C26.0004 28.2 34.1334 33.1253 43.6392 17.9711M26.0004 28.2C26.0004 28.2 12.1989 41.4 20.5998 48.6C29.0008 55.8 47.6003 39.9048 47.6003 39.9048" stroke="white" strokeWidth="2"/>
+                    <ellipse cx="28.4" cy="7.19999" rx="2.4" ry="2.4" fill="#363F55"/>
+                    <ellipse cx="49.3997" cy="39" rx="4.2" ry="4.2" fill="#363F55"/>
+                    <ellipse cx="44.0001" cy="16.8" rx="2.4" ry="2.4" fill="#363F55"/>
+                    <ellipse cx="49.3999" cy="39" rx="3" ry="3" fill="white"/>
+                  </g>
+                  <defs>
+                    <filter id="stetho" x="0" y="0" width="64" height="64" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                      <feFlood floodOpacity="0" result="BackgroundImageFix"/>
+                      <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+                      <feOffset dy="2"/>
+                      <feGaussianBlur stdDeviation="1"/>
+                      <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+                      <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow"/>
+                      <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape"/>
+                    </filter>
+                    <linearGradient id="stetho1" x1="5.21429" y1="60" x2="74.9105" y2="29.5666" gradientUnits="userSpaceOnUse">
+                      <stop offset="0.109375" stopColor="#F14D70"/>
+                      <stop offset="0.317708" stopColor="#FF008A"/>
+                      <stop offset="0.520833" stopColor="#FF446C"/>
+                      <stop offset="0.869792" stopColor="#FFB016"/>
+                    </linearGradient>
+                  </defs>
+                </svg>
                 <h6 className='detail-title'>Physician's Orders for Life-Sustaining Treatment (POLST)</h6>
                 <p className='detail-text'>This is a medical order that tells emergency health care professionals what to do during a medical crisis where the patient cannot speak for him or herself.</p>
                 <ButtonSecondary link={polstForm} download={true} content='Download form' />
