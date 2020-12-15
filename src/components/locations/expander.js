73cbe22e55fbe1d5f9fee56947b7d0b2c7792307
Branon Eusebio <branoneusebio@Branons-MacBook-Pro.local>
2020-12-15 11:21:33 -0800

    Fixed linter errors for expander component in Locations

--- src/components/locations/expander.js
+++ src/components/locations/expander.js
@@ -6,13 +6,17 @@ const Expander = (props) => {
 
 
   //Upon render or rerender, allow for height calculation to be dynamic based on window size
-  const [screenWidth, setScreenWidth] = useState(0);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
   useEffect(() => {
       //upon render, listen for screen size change and setState to 
       if (typeof window !== 'undefined') {
-        window.addEventListener('resize', setScreenWidth(window.innerWidth));
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize); 
       }
-  })
+  });
+  const handleResize = () => {
+    setScreenWidth(window.innerWidth);
+  } 
 
   //calculate the height based on the amount of items in the list
   const heightFactor = `${screenWidth < 450 ? 
@@ -27,15 +31,25 @@ const Expander = (props) => {
   const businesses = [];
   props.cities.map(city => {
     city.locations.map(business => {
-      businesses.push(business);
+      return businesses.push(business);
     })
+    return city;
   })
 
   //render a menu that displays a link for each page in the config file
     return (
         <div className={`Expander ${props.isExpanded ? 'expanded-card' : ''}`} style={{height: `${props.isExpanded ? heightFactor : '0px'}`}}>
           {businesses.map(business => {
-            return <a href={business.link} target='_blank' rel='noopener noreferrer' key={business.name} onMouseOver={() => {handleHover(business.name)}}>{business.name}</a>
+            return <a 
+              href={business.link} 
+              target='_blank' 
+              rel='noopener noreferrer' 
+              key={business.name} 
+              onMouseOver={() => {handleHover(business.name)}}
+              onFocus={() => {handleHover(business.name)}}
+            >
+              {business.name}
+            </a>
           })}
         </div>
     );
