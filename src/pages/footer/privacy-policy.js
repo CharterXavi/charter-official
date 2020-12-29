d7b3ef54ae3545a8d9170e715d97622bac39581b
Branon Eusebio <branoneusebio@Branons-MacBook-Pro.local>
2020-12-29 15:53:38 -0800

    Adding content to empty privacy and terms pages

---
+++ src/pages/footer/privacy-policy.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {Link} from 'gatsby';
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import errorImage from '../images/404.png';
+import './404.css';
+
+const PrivacyPage = () => (
+  <Layout>
+    <SEO title="Privacy Policy" />
+    <div className='error-wrapper'>
+      <img src={errorImage} alt="Error - page not found" />
+      <h3>NOT FOUND</h3>
+      <h6>The page you're looking for doesn't seem to be available. We're sorry for the inconvenience!</h6>
+      <Link to='/'>Return Home</Link>
+    </div>
+  </Layout>
+)
+
+export default PrivacyPage
