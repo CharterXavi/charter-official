9769a2d45a5e94bbafb556a0d2dac04b116812ed
Branon Eusebio <branoneusebio@Branons-MacBook-Pro.local>
2020-12-29 15:05:55 -0800

    Here goes another round of LIghthouse audits

--- src/components/buttons/social-button.js
+++ src/components/buttons/social-button.js
@@ -18,7 +18,7 @@ const SocialButton = (props) => {
         data-aos-duration={props.animationTime}
         >
             {/* if an icon prop is passed, display it */}
-            {props.icon && <img src={props.icon} alt='Button Icon' />}
+            {props.icon && <img src={props.icon} alt='Button Icon' width='30' height='30'/>}
             {/* display the content passed as a prop i.e. - what goes inside the button? */}
             {props.content}
         </a>
