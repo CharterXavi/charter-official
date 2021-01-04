42d0cfcf6c81ef77492212bee59b63b11932c96b
Branon Eusebio <branoneusebio@Branons-MacBook-Pro.local>
2021-01-04 14:15:48 -0800

    Files reappearing, adding to remove once again

---
+++ src/components/ccm/acp.js
@@ -0,0 +1,52 @@
+import React, {useEffect} from 'react';
+import './acp.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
+import ButtonSecondary from '../buttons/button-secondary';
+import directiveForm from '../../images/directive.pdf';
+import polstForm from '../../images/polst.pdf';
+import healthIcon from '../../images/iconography/hospice/healthcare.png';
+import stethoscopeIcon from '../../images/iconography/hospice/stethoscope.png';
+
+const ACP = () => {
+
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  });
+
+  return (
+    <div className='ACP'>
+      <div className='two-column-left'>
+        <div className='left'>
+          <h2>Advanced Care Planning</h2>
+          <p>Our qualified health care professionals work to help you identify your needs, goals and medical treatment preferences so you have a plan in place for advanced care. Once these first steps are completed they will help you select the most appropriate Advanced Care Directive. The goal is to ensure that you select a health care agent that you trust and feel comfortable with and that you receive the treatment you want when you can no longer make those decisions for yourself.</p>
+          <p>Two important forms should be completed during this process:</p>
+          <ul>
+            <li>Physician's Orders for Life-Sustaining Treatment (POLST): a medical order that tells emergency health care professionals what to do during a medical crisis where the patient cannot speak for him or herself.</li>
+            <li>Advanced Health Care Directive: you have the right to give instructions about your own health care. You also have the right to name someone else to make health care decisions for you. This form lets you do either or both of these things. </li>
+          </ul>
+        </div>
+
+        <div className='right forms'>
+            <div className='form-card' data-aos='fade-up' data-aos-duration='800'>
+              <div className='card-title'>
+                <img src={stethoscopeIcon} alt="" />
+                <h5>Physician's Orders for Life-Sustaining Treatment</h5>
+              </div>
+              <ButtonSecondary link={polstForm} download={true} content='Download form' />
+            </div>
+            <div className='form-card' data-aos='fade-up' data-aos-duration='1400'>
+              <div className='card-title'>
+                <img src={healthIcon} alt="" />
+                <h5>Advanced Healthcare Directive</h5>
+              </div>
+              <ButtonSecondary link={directiveForm} download={true} content='Download form' />
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ACP;
---
+++ src/components/video/video.js
@@ -0,0 +1,58 @@
+import React, {useState, useEffect} from 'react';
+import './video.css';
+import videoBlast1 from '../../images/iconography/video-blast1.png';
+import videoBlast2 from '../../images/iconography/video-blast2.png';
+import videoWaveTop from '../../images/video-wave-top.png';
+import videoWaveBottom from '../../images/video-wave-bottom.png';
+import AOS from "aos";
+import "aos/dist/aos.css";
+import ButtonPrimary from '../../components/buttons/button-primary';
+
+const VideoStrip = () => {
+
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  });
+
+  const [videoState, setVideoState] = useState(false);
+  const [buttonState, setButtonState] = useState(false);
+
+  const showVideo = () => {
+    setVideoState(!videoState);
+    setButtonState(!buttonState);
+  }
+
+  return (
+    <div className='VideoStrip'>
+      <img src={videoWaveTop} alt="Wave" className='video-wave-top' />
+      <img src={videoBlast1} alt="group of healthcare symbols" className='video-blast1' data-aos='fade-right' data-aos-duration='1500' />
+
+      <div className='title'>
+        <h2>See the</h2>
+        <h2>Charter Difference</h2>
+        <h2>for yourself</h2>
+      </div>
+
+      <p className={`${videoState ? 'hide' : 'headline'}`} >
+        Learn more about what makes us who we are, from our services to our teams and everything in between.
+      </p>
+
+      <iframe 
+        className={`video ${videoState ? 'active' : ''}`} 
+        src="https://player.vimeo.com/video/312555365" 
+        title='Charter Healthcare Introduction and Information'
+        width="640" height="360" frameBorder="0" allowFullScreen
+      />
+
+      <div className='play-btn-wrapper' onClick={showVideo} onKeyDown={showVideo} role='button' tabIndex='0'>
+        {videoState ? <ButtonPrimary content='Hide Video' /> : <ButtonPrimary content='Watch Video' />}
+      </div>
+
+      <img src={videoWaveBottom} alt="Wave" className='video-wave-bottom' />
+      <img src={videoBlast2} alt="group of healthcare symbols" className='video-blast2' data-aos='fade-left' data-aos-duration='1500' />
+    </div>
+  )
+}
+
+export default VideoStrip
