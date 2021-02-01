dcb87a862f6929d56710caccc30fa9359cb6698c
Branon Eusebio <branoneusebio@Branons-MacBook-Pro.local>
2021-02-01 12:24:46 -0800

    Added and configured gatsby-plugin-netlify for r301 redirects with old website pages to preserve backlinks

--- gatsby-config.js
+++ gatsby-config.js
@@ -99,6 +99,7 @@ module.exports = {
         defer: false,
       },
     },
+    'gatsby-transformer-remark',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
