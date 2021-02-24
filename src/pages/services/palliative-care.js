51bd7d27211dba4305c550e47a1d82cc5ecce22a
Branon Eusebio <branoneusebio@Branons-MacBook-Pro.local>
2021-02-24 12:00:40 -0800

    Made changes for content SEO on Palliative Care Page

--- src/pages/services/palliative-care.js
+++ src/pages/services/palliative-care.js
@@ -12,8 +12,8 @@ import palliativeImage from '../../images/headers/palliative.png';
 const PalliativeCarePage = () => (
   <Layout>
     <div className='PalliativeCarePage'>
-      <SEO title="Palliative Care" />
-      <HeaderStrip2 image={palliativeImage} title='Palliative Care' headline='Equipping people to cope with serious illnesses' />
+      <SEO title="Best Palliative Care | Comfort, Home, and End of Life Care" />
+      <HeaderStrip2 image={palliativeImage} title='Palliative Care' headline='Equipping people to cope with serious illnesses in the comfort of your home' />
       <div className='two-column'>
         <div className="title">
           <h4>Empowering Patients & Improving Quality of Life</h4>
@@ -23,16 +23,17 @@ const PalliativeCarePage = () => (
           <div className="left">
             <h6>What is Palliative Care?</h6>
             <p>
-              Palliative care is a comprehensive care program focused on providing relief from the symptoms and stress of serious illnesses.
-              Our Palliative team, made of specially-trained doctors, NPs, nurses, and other disciplines- can help you and your loved ones navigate and cope with the difficult experience.
+              Palliative care is a comprehensive care program focused on providing relief from the symptoms and stress of serious illnesses. Our Palliative team,
+              made of specially-trained doctors, NPs, nurses, and other disciplines- can help you and your loved ones navigate and cope with the difficult experience.
+              Our best-in-class palliative care is integral in providing comfort in difficult situations such as extreme or end-of-life illnesses.
             </p>
           </div>
           <div className="right">
             <h6>Palliative Care vs. Hospice Care</h6>
             <p>
-              Palliative care is very different from <a href='/services/hospice'>Hospice</a>: it's a program for a much more broad range of patients. It is based on the needs of the patient, 
-              regardless of your age, stage, or prognosis. Palliative care can be a great additional layer upon your primary treatment plan so you are as supported as possible in your
-              treatment, comfort, and quality of life.
+              Palliative care is very different from <a href='/services/hospice'>Hospice Care</a>: it's a program for a much more broad range of patients. It is based on the needs of the patient,
+              regardless of your age, stage, or prognosis. Palliative care or palliative home care can be a great additional layer upon your primary treatment
+              plan so you are as supported as possible in your treatment, comfort, and quality of life.
             </p>
           </div>
         </div>
